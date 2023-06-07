0bc7080fad63811e4d5ffafb2a9d3b82ec5c25df
zayniel <98770698+zayniel@users.noreply.github.com>
2023-06-07 16:41:52 -0400

    Design change + proj. experience

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import pfp from './pfp.jpg';
 import border from './border.jpg';
 import Blur from "react-blur";
 import resume from './Resume (2).jpg';
+import darkpge from './pge-assets/pge-dark-mode.PNG'
 
 function App() {
   return (
     
-    <div className="App"> 
+  <div className="App"> 
     <br></br> 
     <br></br> 
     <br></br>   
@@ -35,22 +36,39 @@ function App() {
         </a>
         </p>
         
-  </header>
+    </header>
 
 	  <div className="about-me">About Me</div>
-	  <div className="about-me-text">Hi! My name is Zayne and I am a Senior Software Engineering major at Michigan Technological University. I have several hobbies such as playing music (drums, guitar, piano), playing video games, watching movies, and learning how to make various crafts. Below you can find more on some of the projects I have worked on during my time here at Tech.</div>
+	  <div className="about-me-text">I am an aspiring software engineer with a focus on learning web development. 
+    <br></br> <br></br>
+    I have several hobbies such as playing instruments, video games, watching movies, and various crafts.
+    <br></br> <br></br>
+    Below you can find some of the projects I've been involved in.</div>
     
-    <div className="resume-text">View My Resume</div>
+    <div className="resume-text">View Resume</div>
     
       <a href={resume} target="_blank">
         <img src={resume} className="resume" alt="resume" />
       </a>
 
-        <div className="container">
-          <div className="gallery"></div>
-        </div>
+      <div className="container">
+        <div className="gallery"></div>
+      </div>
 
-    </div>
+      <div className="project-header">Projects</div>
+
+      <div className="pge-header">PG&E Energy Reducer</div>
+      <div className="pge-desc">
+        The PG&E Energy Reducer is a web application for Pacific Gas & Electric customers that allows them to visualize their heat and electric consumption through easily readable and understandable graphical representations. The app also shows trends and recurring energy spikes and gives the customer advice to help reduce their energy consumption as well as their energy costs.
+        <br></br> <br></br>
+        Role: Developer
+      </div>
+      
+      <img src={darkpge} className="pge-content" alt="pgedark" />
+
+      <div className="construction">In Construction...</div>
+
+  </div>
   );
 }
 
