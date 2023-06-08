1014b3ceb0a8561bd92bd6a1c5153fd45696f74c
zayniel <98770698+zayniel@users.noreply.github.com>
2023-06-08 14:19:28 -0400

    Fixed small browser view

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
     </header>
 
 	  <div className="about-me">About Me</div>
-	  <div className="about-me-text">I am an aspiring software engineer with a focus on learning web development. 
+	  <div className="about-me-text">I am an aspiring software engineer with a focus on web development. 
     <br></br> <br></br>
     I have several hobbies such as playing instruments, video games, watching movies, and various crafts.
     <br></br> <br></br>
@@ -62,9 +62,16 @@ function App() {
         The PG&E Energy Reducer is a web application for Pacific Gas & Electric customers that allows them to visualize their heat and electric consumption through easily readable and understandable graphical representations. The app also shows trends and recurring energy spikes and gives the customer advice to help reduce their energy consumption as well as their energy costs.
         <br></br> <br></br>
         Role: Developer
+        <br></br><br></br>
+        <a
+        href="https://www.linkedin.com/in/zayne-pepin-546a38220/"
+        target="_blank"
+        rel="noopener noreferrer">
+          pge-app.sachinf.com
+        </a>
       </div>
-      
       <img src={darkpge} className="pge-content" alt="pgedark" />
+      
 
       <div className="construction">In Construction...</div>
 
